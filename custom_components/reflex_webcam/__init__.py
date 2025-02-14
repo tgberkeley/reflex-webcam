5429ef235b72b0c2f43f69d6391bee9e38fdb266
Masen Furer <m_github@0x26.net>
2025-02-14 12:54:16 -0800

    Update code for 0.7.0

--- custom_components/reflex_webcam/__init__.py
+++ custom_components/reflex_webcam/__init__.py
@@ -1,1 +1,7 @@
-from .webcam import *
+from .webcam import strip_codec_part, upload_screenshot, webcam
+
+__all__ = [
+    "strip_codec_part",
+    "upload_screenshot",
+    "webcam",
+]
--- webcam_demo/webcam_demo/webcam_demo.py
+++ webcam_demo/webcam_demo/webcam_demo.py
@@ -1,12 +1,12 @@
 """Take screenshots and video recordings from webcam."""
+
 import time
 from pathlib import Path
 from urllib.request import urlopen
-from PIL import Image
 
 import reflex as rx
-import reflex_webcam as webcam
-
+from PIL import Image
+from reflex_webcam import strip_codec_part, upload_screenshot, webcam
 
 # Identifies a particular webcam component in the DOM
 WEBCAM_REF = "webcam"
@@ -25,68 +25,71 @@ SOURCE_CODE = [
 rx.upload()
 
 
+SCREENSHOT_TAB = "screenshot_tab"
+VIDEO_TAB = "video_tab"
+
+
 class State(rx.State):
     last_screenshot: Image.Image | None = None
     last_screenshot_timestamp: str = ""
     loading: bool = False
     recording: bool = False
+    n_recordings: int = 0  # incremented to cache bust the same video name
+    active_tab: str = SCREENSHOT_TAB
 
+    @rx.event
+    def set_active_tab(self, tab_value: str):
+        self.active_tab = tab_value
+
+    @rx.event
     def handle_screenshot(self, img_data_uri: str):
         """Webcam screenshot upload handler.
         Args:
             img_data_uri: The data uri of the screenshot (from upload_screenshot).
         """
-        if self.loading:
+        if self.loading or not img_data_uri:
             return
         self.last_screenshot_timestamp = time.strftime("%H:%M:%S")
         with urlopen(img_data_uri) as img:
             self.last_screenshot = Image.open(img)
             self.last_screenshot.load()
             # convert to webp during serialization for smaller size
-            self.last_screenshot.format = "WEBP"  # type: ignore
+            self.last_screenshot.format = "WEBP"
+        self.active_tab = SCREENSHOT_TAB
 
     def _video_path(self) -> Path:
-        return Path(rx.get_upload_dir()) / VIDEO_FILE_NAME
+        return Path(rx.get_upload_dir()) / self.video_path
 
     @rx.var(cache=True)
-    def video_exists(self) -> bool:
-        if not self.recording:
-            return self._video_path().exists()
-        return False
+    def video_path(self) -> str:
+        return f"{self.router.session.client_token}_{VIDEO_FILE_NAME}"
 
+    @rx.var(cache=True, deps=["recording"])
+    def video_exists(self) -> bool:
+        return self._video_path().exists()
+
+    @rx.event
     def on_start_recording(self):
         self.recording = True
-        print("Started recording")
+        self.n_recordings += 1
+        print("Started recording")  # noqa: T201
         with self._video_path().open("wb") as f:
             f.write(b"")
 
-    def _strip_codec_part(self, chunk: str) -> str:
-        parts = chunk.split(";")
-        for part in parts:
-            if "codecs=" in part:
-                parts.remove(part)
-                break
-        return ";".join(parts)
-
+    @rx.event
     def handle_video_chunk(self, chunk: str):
-        print("Got video chunk", len(chunk))
-        with self._video_path().open("ab") as f:
-            with urlopen(self._strip_codec_part(chunk)) as vid:
-                f.write(vid.read())
+        print("Got video chunk", len(chunk))  # noqa: T201
+        with (
+            self._video_path().open("ab") as f,
+            urlopen(strip_codec_part(chunk)) as vid,
+        ):
+            f.write(vid.read())
 
+    @rx.event
     def on_stop_recording(self):
-        print(f"Stopped recording: {self._video_path()}")
+        print(f"Stopped recording: {self._video_path()}")  # noqa: T201
         self.recording = False
-
-    def start_recording(self, ref: str):
-        """Start recording a video."""
-        return webcam.start_recording(
-            ref,
-            on_data_available=State.handle_video_chunk,
-            on_start=State.on_start_recording,
-            on_stop=State.on_stop_recording,
-            timeslice=1000,
-        )
+        self.active_tab = VIDEO_TAB
 
 
 def last_screenshot_widget() -> rx.Component:
@@ -114,36 +117,58 @@ def webcam_upload_component(ref: str) -> rx.Component:
         A reflex component.
     """
     return rx.vstack(
-        webcam.webcam(
+        webcam_obj := webcam(
             id=ref,
-            on_click=webcam.upload_screenshot(
-                ref=ref,
-                handler=State.handle_screenshot,  # type: ignore
-            ),
             audio=True,
+            timeslice=1000,
+            on_click=upload_screenshot(webcam_id=ref, handler=State.handle_screenshot),
+            on_data_available=State.handle_video_chunk,
+            on_start=State.on_start_recording,
+            on_stop=State.on_stop_recording,
         ),
         rx.cond(
             ~State.recording,
             rx.button(
                 "🟢 Start Recording",
-                on_click=State.start_recording(ref),
+                on_click=webcam_obj.start(),
                 color_scheme="green",
                 size="4",
             ),
             rx.button(
                 "🟤 Stop Recording",
-                on_click=webcam.stop_recording(ref),
+                on_click=webcam_obj.stop(),
                 color_scheme="tomato",
                 size="4",
             ),
         ),
-        rx.cond(
-            State.video_exists,
-            rx.link(
-                "Download Last Video", href=rx.get_upload_url(VIDEO_FILE_NAME), size="4"
+        rx.tabs.root(
+            rx.tabs.list(
+                rx.cond(
+                    State.last_screenshot,
+                    rx.tabs.trigger("Screenshot", value=SCREENSHOT_TAB),
+                ),
+                rx.cond(
+                    State.video_exists,
+                    rx.tabs.trigger("Video", value=VIDEO_TAB),
+                ),
             ),
+            rx.tabs.content(
+                last_screenshot_widget(),
+                value=SCREENSHOT_TAB,
+            ),
+            rx.tabs.content(
+                rx.video(
+                    url=f"{rx.get_upload_url(State.video_path)}?r={State.n_recordings}",
+                    playing=True,
+                    width="320px",
+                    key=State.recording,
+                ),
+                value=VIDEO_TAB,
+            ),
+            value=State.active_tab,
+            on_change=State.set_active_tab,
+            width="320px",
         ),
-        last_screenshot_widget(),
         width="320px",
         align="center",
     )
